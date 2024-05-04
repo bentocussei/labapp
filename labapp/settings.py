03b5e59eec63075a0563103f7204c7c034236901
Bento Cussei <becussei@gmail.com>
2024-05-04 10:31:16 +0100

    Update README.md and settings SECRET_KEY moved

--- labapp/settings.py
+++ labapp/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*hylsghgx7x8^a0-a4y9r*k1811_)h%%p&z8ewl83-pw@m0f0_'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
